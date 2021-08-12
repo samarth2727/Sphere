cdb97cc454e9c343bc0a68c922e17df3ea12284d
n-ce <abrocks1120@gmail.com>
2021-08-12 22:59:40 +0530

    Tab Color, Smoothness Improved

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ window.onload = (event) => {
   var R = Number(Math.floor(Math.random()*255)).toString(16);
   var G = Number(Math.floor(Math.random()*255)).toString(16);
   var B = Number(Math.floor(Math.random()*255)).toString(16);
-  document.getElementById('a').style.backgroundColor="#"+R+G+B;
-  document.body.style.backgroundColor="#"+R+G+B;
+  document.querySelector('div').style.backgroundColor= document.body.style.backgroundColor="#"+R+G+B;
+  document.querySelector("meta[name=theme-color]").setAttribute("content","#"+R+G+B);
   }, 1000);
 }
+function fullscreen()
+{document.documentElement.requestFullscreen();}
