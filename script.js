c9fc50c39689dcb699cf8b812673263b4ff71ebe
n-ce <abrocks1120@gmail.com>
2021-08-12 21:06:14 +0530

    Added Shadow, rem units for dimensions

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ window.onload = (event) => {
   var B = Number(Math.floor(Math.random()*255)).toString(16);
   document.getElementById('a').style.backgroundColor="#"+R+G+B;
   document.body.style.backgroundColor="#"+R+G+B;
-  }, 5000);
+  }, 1000);
 }
